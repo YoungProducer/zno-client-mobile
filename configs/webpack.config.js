3560dac2f05a775e6b02f3db87c98469bf559b2c
YoungProducer <sashabezrukovownmail@gmail.com>
2020-03-28 13:20:24 +0200

    Added new environment variable - ANALYZE_BUNDLE which related for BundleAnalyzerWebpackPlugin.

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -12,6 +12,8 @@ module.exports = function getWebpackConfig(mode) {
 
   const src = paths.appSrc;
 
+  const analyzeBundle = process.env.ANALYZE_BUNDLE === 'true';
+
   return {
     entry: paths.appIndexJs,
     devtool: "source-map",
@@ -107,7 +109,7 @@ module.exports = function getWebpackConfig(mode) {
         // safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
         // systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
       }),
-      new BundleAnalyzerPlugin(),
+      analyzeBundle && new BundleAnalyzerPlugin(),
       production && new webpack.IgnorePlugin(/redux-logger/)
     ].filter(Boolean)
   };
